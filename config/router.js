33772bc8057f65bbdb88ac394bc35ed23503719d
switch120 <scott.byers@aethercode.com>
2017-08-14 17:31:04 -0400

    - router fix

--- config/router.js
+++ config/router.js
@@ -33,21 +33,23 @@ module.exports = function(app)
             .WhereKeywords(searchParams)
             .Limit(25)
             .UserIP("1.2.3.4")
-            .UserAgent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36")
-            .Search(
-                function (results) {
-                    // do something with the success results
-                    res.send(results);
-                },
-                function (error) {
-                    // do something with the error results
-                    console.log(error);
-                });
+            .UserAgent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36");
 
         if (req.body.postal_code) sObj.WhereLocation({ postalCode: req.body.postal_code });
 
         // add radius from zip if both postalCode and Radius specified
         if (req.body.radius && req.body.postal_code) sObj.Radius(req.body.radius);
+
+        sObj.Search(
+            function (results) {
+                // do something with the success results
+                res.send(results);
+            },
+            function (error) {
+                // do something with the error results
+                console.log(error);
+            }
+        );
     });
 
     // Set url for API group routes
